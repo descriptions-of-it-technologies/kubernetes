597b23fc32526b1435a1a21a6340b62a879f1437
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-09-02 15:55:04 +0200

    Adding new information.

--- prepare-to-certification/commands.sh
+++ prepare-to-certification/commands.sh
@@ -13,9 +13,55 @@ kubectl run nginx --image=nginx --dry-run=client -o yaml > pod.yaml
 # Create yaml file of pod configuration.
 kubectl run nginx --generator=run-pod/v1 --image=nginx --dry-run=client -o yaml > pod.yaml
 
+# Create yaml file of pod configuration with command env.
+kubectl run busybox --image=busybox --restart=Never --dry-run -o yaml --command -- env > pod.yaml
+# This command does not work as expected.
+kubectl run busybox --image=busybox --restart=Never --dry-run --command -- env -o yaml > pod.yaml
 # Create yaml file of pod configuration with command sleep.
 kubectl run nginx --image=nginx --dry-run=client -o yaml --command -- sleep 300 > pod.yaml
+# Create pod busybox with command echo. When the pod terminated it be removed.
+kubectl run busybox --image=busybox --restart=Never --rm -it --command -- echo 'Hello World !!!'
 
+# Create pod with command env.
+kubectl run busybox --image=busybox --restart=Never -it --command -- env
+
+# Create pod with namespace.
+kubectl run nginx --image=nginx -n=mynamespace
+kubectl run nginx --image=nginx --namespace=mynamespace
+
+# Create yaml file of pod configuration. Not work flag namespace.
+kubectl run nginx --image=nginx -n=mynamespace --dry-run=client -o yaml > pod-3.yaml
+kubectl run nginx --image=nginx --namespace=mynamespace --dry-run=client -o yaml > pod-3.yaml
+
+# Create pod with labels.
+kubectl run nginx --image=nginx --restart=Never --labels=app=v1
+# Overwrite label.
+kubectl label pod nginx2 key=newValue --overwrite
+# Get all pods by label.
+kubectl get pod -l=app
+kubectl get pod -l=app=v1
+kubectl get pod -l 'app in (v1)'
+kubectl get po --selector=app=v1
+kubectl get pod -L=app
+kubectl get pod --label-columns=app
+# Get all pods that not have the label.
+kubectl get pod -l='!app'
+# Delete label from pod.
+kubectl label pod namePod key-
+
+# Add annotations to pod.
+kubectl annotate pod namePod description='Some important description.'
+# Remove the annotation of pod.
+kubectl annotate pod namePod key-
+
+# Create pod with pot 80.
+kubectl run nginx --image=nginx --restart=Never --port=80
+
+# Change pod's image to nginx:1.7.1.
+kubectl set image pod/nginx nginx=nginx:1.7.1
+
+#
+kubectl run busybox --image=busybox --rm -it --restart=Never -- wget -O- 10.244.1.4:80
 
 kubectl label pod [podName] [key]=[value]
 kubectl label pod [podName] [key]=[value] --overwrite
@@ -31,6 +77,12 @@ kubectl label pod [podName] [key]-
 # ====================================================================================================================== Deployment
 # Create yaml file of deployment configuration.
 kubectl create deploy nginx --image=nginx --dry-run=client -o yaml > deployment.yaml
+
+# Update deployment image.
+kubectl set image deploy nginx nginx=nginx:1.7.9
+kubectl set image deploy nginx nginx=nginx:1.7.9 --record
+kubectl edit deploy nginx
+kubectl edit deploy nginx --record
 
 # Scale Deployment of 6 replicas.
 kubectl scale deploy nginx --replicas=6
@@ -87,7 +139,11 @@ echo 'MWYyZDFlMmU2N2Rm' | base64 --decode
 # If a field, such as username, is specified in both data and stringData, the value from stringData is used.
 
 # ====================================================================================================================== Namespace
+# Create namespace.
 kubectl create namespace [nameNamespace]
+
+# Create yaml file of namespace configuration.
+kubectl create namespace myns --dry-run -o yaml > ns.yaml
 
 # ====================================================================================================================== Config
 kubectl config current-context
@@ -100,6 +156,28 @@ kubectl config delete-context
 # ====================================================================================================================== Job
 
 # ====================================================================================================================== CronJob
+
+# ====================================================================================================================== ResourceQuota
+# A resource quota, defined by a ResourceQuota object, provides constraints that limit aggregate resource consumption
+# per namespace. It can limit the quantity of objects that can be created in a namespace by type, as well as the total
+# amount of compute resources that may be consumed by resources in that project.
+
+# Create ResourceQuota with hard limits of 1 CPU, 3Gi memory.
+kubectl create quota myrs --hard=cpu=1,memory=3Gi
+
+# Create yaml config file of ResourceQuota with hard limits of 1 CPU, 1G memory and 2 pods.
+kubectl create         quota myrq --hard=cpu=1,memory=1Gi,pods=2 --dry-run -o yaml > rq.yaml
+kubectl create resourcequota myrq --hard=cpu=1,memory=1Gi,pods=2 --dry-run -o yaml > rq.yaml
+
+# Create yaml config file of ResourceQuota.
+kubectl create quota myrq --hard=pods=2,cpu=1,requests.cpu=3,limits.cpu=1,memory=200Gi,requests.memory=3Gi,limits.memory=10Gi --dry-run=client -o yaml > rs.yaml
+
+# ====================================================================================================================== LimitRanges
+# By default, containers run with unbounded compute resources on a Kubernetes cluster. With resource quotas, cluster
+# administrators can restrict resource consumption and creation on a namespace basis. Within a namespace, a Pod or
+# Container can consume as much CPU and memory as defined by the namespace's resource quota. There is a concern that
+# one Pod or Container could monopolize all available resources. A LimitRange is a policy to constrain resource
+# allocations (to Pods or Containers) in a namespace.
 
 # ====================================================================================================================== ServiceAccount
 kubectl create serviceaccount [nameServiceAccount]
@@ -118,6 +196,9 @@ kubectl logs podName
 
 kubectl logs podName -c containerName
 
+# If pod crashed and restarted, get logs about the previous instance
+kubectl logs nginx -p
+
 # ====================================================================================================================== Events
 
 kubectl get events
@@ -128,6 +209,8 @@ kubectl explain cronjob.spec.jobTemplate --recursive
 # ====================================================================================================================== Exec
 
 kubectl exec -it podName -- /bin/bash
+
+kubectl exec -it podName -- /bin/sh
 
 kubectl exec -it podName -c containerName -- /bin/bash
 
